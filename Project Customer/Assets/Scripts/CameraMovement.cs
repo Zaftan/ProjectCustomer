3bef6c1bf41a1c5be704372be806a120ab33dcc3
Brynn <brynn.rothuis@hotmail.com>
2021-09-01 16:07:04 +0200

    First Person Controller done

---
+++ Project Customer/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+    public Transform body;
+
+    float xAxisRotation = 0f;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xAxisRotation -= mouseY;
+        xAxisRotation = Mathf.Clamp(xAxisRotation, -50f, 50f);
+
+        transform.localRotation = Quaternion.Euler(xAxisRotation, 0f, 0f);
+        body.Rotate(Vector3.up * mouseX);
+    }
+}
