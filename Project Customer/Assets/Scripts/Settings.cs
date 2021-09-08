b74196950d8f62005b6c42e1166e173262ac4265
Brynn <brynn.rothuis@hotmail.com>
2021-09-08 13:24:08 +0200

    Added setting and getting from and to player prefs

--- Project Customer/Assets/Scripts/Settings.cs
+++ Project Customer/Assets/Scripts/Settings.cs
@@ -7,13 +7,23 @@ public class Settings : MonoBehaviour
 {
     public AudioMixer audioMixer, audioMixer2;
 
+    private void Start()
+    {
+        PlayerPrefs.GetFloat("BackgroundMusic");
+        PlayerPrefs.GetFloat("SFX");
+    }
+
     public void SetVolume(float volume)
     {
         audioMixer.SetFloat("1 - Background", volume);
+        PlayerPrefs.SetFloat("BackgroundMusic", volume);
+        PlayerPrefs.Save();
     }
 
     public void SetSFX(float volume)
     {
         audioMixer2.SetFloat("2 - SFX", volume);
+        PlayerPrefs.SetFloat("SFX", volume);
+        PlayerPrefs.Save();
     }
 }
